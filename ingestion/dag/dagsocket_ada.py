a325812910346eda214e8bc83c3c457ca805a04a
Valipsy <Valipsy@github.com>
2021-12-15 13:43:21 +0100

    Premier jet pour automatiser l'ingestion du websocket ADA

---
+++ ingestion/dag/dagsocket_ada.py
@@ -0,0 +1,60 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from dotenv import load_dotenv  # Permet le stockage de données sensible dans un fichier à part
+import os                       # Fonction liée à l'operating system
+import websocket, json
+from json import loads
+from elasticsearch import Elasticsearch
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+IP_ES = os.getenv("IP_ES")
+cc = "adausdt"
+socket = f"wss://stream.binance.com:9443/ws/{cc}@trade"
+
+es = Elasticsearch([IP_ES])
+
+default_args = {
+    'owner': 'Mangouste',
+    'start_date': datetime(2021,12,15),
+    "retries": 1,
+    "retry_delay": timedelta(seconds=5)
+}
+
+dag = DAG(
+    "tutorial",
+    default_args=default_args,
+    start_date=datetime(2015, 12, 1),
+    description="Extraction du cours de l'ADA",
+    schedule_interval='* * * * * *',
+    catchup=False
+)
+
+XYZ = DAG('websocket_ada', default_args=default_args)
+
+####### Definition des Fonctions / Executions #######
+
+def on_message(ws, message):
+    json_message = json.loads(message)
+    price_int = json_message['p']
+    price = float(price_int)
+    doc = {
+    'exchanger' : 'Binance',
+    'price_int' : price,
+    'symbol' : json_message['s'],
+    'timestamp' : datetime.now()
+    }
+    es.index(index="test_websocket", document=doc)
+    ws = websocket.WebSocketApp(socket, on_message = on_message)
+    ws.run_forever()
+
+P1 = PythonOperator(
+    task_id='Query websocket',
+    python_callable=on_message,
+    dag=XYZ
+)
+
+####### Definition des Etapes #######
+
+P1
